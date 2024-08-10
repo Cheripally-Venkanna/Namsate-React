1e9255a898e4c1cd3f884342d04ac3f954d02462
Cheripally-Venkanna <venkatcheripally1616@gmail.com>
2024-08-10 21:35:31 +0530

    app-1

---
+++ App.js
@@ -0,0 +1,11 @@
+    import React from "react";
+    import ReactDOM from "react-dom/client" ;
+
+    const Ele = ()=>(
+    <div>
+
+        <h1>cheripally from react component</h1>
+
+        </div>);
+        const root = ReactDOM.createRoot(document.getElementById("main"));
+        root.render(<Ele/>);
