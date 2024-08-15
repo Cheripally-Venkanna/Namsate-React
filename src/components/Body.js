1bc6a617fb43915f00a4dbb4b1d0556df4a59534
Cheripally-Venkanna <venkatcheripally1616@gmail.com>
2024-08-15 13:17:38 +0530

    Ep-6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,29 +1,41 @@
 import Container from "./Container";
-import objdata from "../utils/apidata";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import FakeComponent from "./FakeComponent";
 
 
 
 const Body = ()=>{    
-    const [resdata,setresdata] = useState(objdata); 
-    
-
+    const [resdata,setresdata] = useState([]); 
+    useEffect(
+        ()=>{
+            fetchdata();
+        },[]);
+    const fetchdata = async ()=>{
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.96340&lng=77.58550&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        setresdata(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    }
+    if(resdata.length===0){
+        return (
+        
+        <FakeComponent/>)
+    }
     return (
         <div className="body">
             <div className="filter">
                 <button className="btn" onClick={()=>{
-            const filterdata = resdata.filter(
-                (res)=> res.info.avgRating > 4.6
-            );
-            setresdata(filterdata);
-                }}
-                 >
+                   const newdata = resdata.filter((res)=>res.info.avgRating>4.5);
+                  setresdata(newdata); 
+                }} >
                     High Rating Food Cards</button>
                 </div>
         <div className="res-container">
-         { resdata.map((data)=>
-            ( <Container key={data.info.id} prop={data}/> )
-            )}
+            {  
+               resdata.map((res)=>{
+                
+        return (<Container key={res.info.id} prop={res}/>)
+  }  )
+}
         </div>
         </div>
     )
---
+++ src/components/FakeComponent.js
@@ -0,0 +1,23 @@
+const FakeComponent = ()=>{
+    return (
+        <div>
+            <button className="highfood"> High Rating Food Cards</button>
+        <div className="fakemain">
+            <div className="fakerestro"></div>
+            <div className="fakerestro"></div>
+            <div className="fakerestro"></div>
+            <div className="fakerestro"></div>
+            <div className="fakerestro"></div>
+            <div className="fakerestro"></div>
+            <div className="fakerestro"></div>
+            <div className="fakerestro"></div>
+            <div className="fakerestro"></div>
+            <div className="fakerestro"></div>
+            <div className="fakerestro"></div>
+            <div className="fakerestro"></div>   
+        </div>
+        </div>
+
+    )
+}
+export default FakeComponent;
